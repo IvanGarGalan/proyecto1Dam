285af4b9118f5fb80ab71e1bb118cb0ee3a35d22
IvanGarGalan <ivangargalan@gmail.com>
2026-02-16 18:48:21 +0100

    Primer commit,creación del sistema de archivos y primeros archivos de Base de Datos

---
+++ bin/main.dart
@@ -0,0 +1,6 @@
+import './entities/entities.dart';
+import './utils/utils.dart';
+
+void main(List<String> args) async {
+  await Database.instalacionDB();
+}                             
---
+++ bin/utils/database.dart
@@ -0,0 +1,37 @@
+import 'package:mysql1/mysql1.dart';
+
+abstract class Database {
+  static final String _host = "localhost";
+  static final int _port = 3306;
+  static final String _user = "root";
+
+  //creacion de la base de datos cuando la aplicacion se inicia por primera vez
+  static instalacionDB() async {
+    var settings = ConnectionSettings(host: _host, port: _port, user: _user);
+    var conn = await MySqlConnection.connect(settings);
+    try {
+      await _crearDB(conn);
+      await _crearTablaUsuarios(conn);
+    } catch (e) {
+      print(e);
+    } finally {
+      await conn.close();
+    }
+  }
+
+  static _crearDB(conn) async {
+    await conn.query('CREATE DATABASE IF NOT EXISTS proyectoDB');
+    await conn.query('USE proyectoDB');
+    //print('Base de datos creada');
+  }
+
+  static _crearTablaUsuarios(conn) async {
+    await conn.query('''CREATE TABLE IF NOT EXISTS usuarios(
+    idUsuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombreReal VARCHAR(50) NOT NULL,
+    nombreUsuario VARCHAR(50) NOT NULL UNIQUE,
+    contrasenna VARCHAR(50) NOT NULL
+    )''');
+    //print("Tabla de usuarios creada");
+  }
+}
