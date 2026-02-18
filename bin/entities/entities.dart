a7c9a957b525a58c9ac2558104e62c3609ac2a28
IvanGarGalan <ivangargalan@gmail.com>
2026-02-18 17:12:28 +0100

    creacion de la clase usuario y el metodo inicioSesion

--- bin/entities/entities.dart
+++ bin/entities/entities.dart
@@ -1,1 +1,2 @@
 export 'menu.dart';
+export 'usuario.dart';
---
+++ bin/entities/usuario.dart
@@ -0,0 +1,7 @@
+abstract class Usuario {
+  String? _nombreUsuario;
+  String? _contrasenna;
+
+  //metodo para iniciar sesion en la aplicacion
+  static void iniciarSesion() {}
+}
