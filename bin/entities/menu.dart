c1b3f35ea122b04905e978d8ba4f52add3251714
IvanGarGalan <ivangargalan@gmail.com>
2026-02-18 16:58:18 +0100

    Crecion del menu inicio en menu.dart

--- bin/entities/menu.dart
+++ bin/entities/menu.dart
@@ -1,6 +1,23 @@
 import 'dart:io';
 
 abstract class Menu {
+  static void menuOpciones() {
+    menuInicio();
+    String opcion = stdin.readLineSync() ?? "0";
+    switch (opcion) {
+      case "1":
+        stdout.writeln("Opcion 1 seleccionada");
+        break;
+
+      case "2":
+        stdout.writeln("Opcion 2 seleccionada");
+        exit(0); //salida del menu de la apllcacion y del menu
+      default:
+        stdout.writeln("Opcion no reconocida,vuelve a intentarlo");
+    }
+  }
+
+  //metodo que muestra el primer menu de la aplicación,inicio de sesión o salir
   static void menuInicio() {
     stdout.writeln('''
     Elige una opción:
