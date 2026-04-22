b345c0556d29c94274a2a31d844a602e330df467
IvanGarGalan <ivangargalan@gmail.com>
2026-04-22 17:26:07 +0200

    creacion de .datosAPI en playablecharacter

--- bin/entities/character.dart
+++ bin/entities/character.dart
@@ -4,52 +4,54 @@ import 'package:http/http.dart' as http;
 import '../utils/api.dart';
 
 class PlayableCharacter {
-  String nombrePersonaje;
-  int fuerza;
-  int destreza;
-  int constitucion;
-  int inteligencia;
-  int sabiduria;
-  int carisma;
-  String clase;
-  int experiencia;
-  String raza;
-  int puntosVida;
-  List<String> equipoInicial;
-  String trasfondo;
+  String? nombrePersonaje;
+  int? fuerza;
+  int? destreza;
+  int? constitucion;
+  int? inteligencia;
+  int? sabiduria;
+  int? carisma;
+  String? clase;
+  int? experiencia;
+  String? raza;
+  int? puntosVida;
+  List<String>? equipoInicial;
+  String? trasfondo;
   List<String>? hechizos;
-  String alineamiento;
-  int puntosGolpe;
-  int iniciativa;
-  int armadura;
-  Map velocidad;
-  String idioma;
+  String? alineamiento;
+  int? puntosGolpe;
+  int? iniciativa;
+  int? armadura;
+  Map? velocidad;
+  String? idioma;
+  int? nivel;
 
-  PlayableCharacter(
-    this.nombrePersonaje,
-    this.clase,
-    this.fuerza,
-    this.destreza,
-    this.constitucion,
-    this.inteligencia,
-    this.sabiduria,
-    this.carisma,
-    this.experiencia,
-    this.raza,
-    this.puntosVida,
-    this.equipoInicial,
-    this.trasfondo,
-    this.hechizos,
-    this.alineamiento,
-    this.puntosGolpe,
-    this.iniciativa,
-    this.armadura,
-    this.velocidad,
-    this.idioma,
-  );
+  PlayableCharacter();
 
   //constructor que prepara los datos de la API al objeto personaje
-  PlayableCharacter.datosAPI(datos) {}
+  PlayableCharacter.datosAPI(datos) {
+    nombrePersonaje = 'abcd'; // to do: poner variable con nombre
+    clase = datos['name'];
+    fuerza = 10;
+    destreza = 10;
+    constitucion = 10;
+    inteligencia = 10;
+    sabiduria = 10;
+    carisma = 10;
+    experiencia = 0;
+    raza = 'Humano'; //to do: poner raza
+    puntosVida = 8; //to do: poner puntos de vida con constitucion
+    equipoInicial = [];
+    trasfondo = ''; // to do: poner transfondo
+    hechizos = [];
+    alineamiento = ''; // to do: poner alineamiento
+    puntosGolpe = datos['hit_die'];
+    iniciativa = 0; // to do: poner iniciativa(destreza + d20)
+    armadura = 0; // to do: poner armadura (10 + destreza)
+    velocidad = {30, 'ft'} as Map<dynamic, dynamic>;
+    idioma = 'Común';
+    nivel = 1;
+  }
 
   //metodo que trae los datos de clase de api y ayuda a crear la clase character
   static Future<PlayableCharacter?> obtenerPersonaje(String clase) async {
