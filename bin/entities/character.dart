fb7ac65a675af85885301f566949fe82140c17bf
IvanGarGalan <ivangargalan@gmail.com>
2026-04-22 16:14:14 +0200

    creacion basica de la clase character

--- bin/entities/character.dart
+++ bin/entities/character.dart
@@ -1,1 +1,30 @@
-class Character {}
+class Character {
+  String nombre;
+  int fuerza;
+  int destreza;
+  int constitucion;
+  int inteligencia;
+  int sabiduria;
+  int carisma;
+  String clase;
+  int experiencia;
+  String raza;
+  int puntosVida;
+
+  Character(
+    this.nombre,
+    this.fuerza,
+    this.destreza,
+    this.constitucion,
+    this.inteligencia,
+    this.sabiduria,
+    this.carisma,
+    this.clase,
+    this.experiencia,
+    this.raza,
+    this.puntosVida,
+  );
+
+  //metodo que trae los datos de clase de api y ayuda a crear la clase character
+  static Future<Character?> obtenerPersonaje() async {}
+}
